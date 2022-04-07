bd87afd12fa218598a810211229404bb36dbf30b
Tequila-Sunrise <steven.shao6174@gmail.com>
2022-04-07 20:58:15 +0800

    Update datasets

--- Project1/code/lattevision/datasets.py
+++ Project1/code/lattevision/datasets.py
@@ -99,22 +99,6 @@ class MNIST(VisionDataset):
         plt.axis('off')
         plt.show()
 
-    @staticmethod
-    def label() -> Dict[int, str]:
-        labels = {
-            0: '0',
-            1: '1',
-            2: '2',
-            3: '3',
-            4: '4',
-            5: '5',
-            6: '6',
-            7: '7',
-            8: '8',
-            9: '9',
-        }
-        return labels
-
 
 #########################################################################################
 #                                        Utility                                        #
