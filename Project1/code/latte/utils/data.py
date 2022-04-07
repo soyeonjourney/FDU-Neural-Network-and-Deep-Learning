fa12f3306aa44765591131f7079dccea1f2c84dc
Tequila-Sunrise <steven.shao6174@gmail.com>
2022-04-07 20:57:58 +0800

    Update data

--- Project1/code/latte/utils/data.py
+++ Project1/code/latte/utils/data.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import List, Tuple, Optional, Generator
+from typing import List, Tuple
 
 
 #########################################################################################
