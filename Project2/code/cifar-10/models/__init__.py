1eb31046e37d8e259f988d34c12d4bb64acce101
Tequila-Sunrise <steven.shao6174@gmail.com>
2022-04-21 18:18:33 +0800

    Update models

---
+++ Project2/code/cifar-10/models/__init__.py
@@ -0,0 +1,35 @@
+from .densenet import (
+    DenseNet,
+    DenseNet121,
+    DenseNet169,
+    DenseNet201,
+    DenseNet161,
+    densenet_cifar,
+)
+from .dla_simple import SimpleDLA
+from .dla import DLA
+from .dpn import DPN, DPN26, DPN92
+from .preact_resnet import (
+    PreActResNet,
+    PreActResNet18,
+    PreActResNet34,
+    PreActResNet50,
+    PreActResNet101,
+    PreActResNet152,
+)
+from .resnet import (
+    ResNet,
+    ResNet18,
+    ResNet34,
+    ResNet50,
+    ResNet101,
+    ResNet152,
+)
+from .resnext import (
+    ResNeXt,
+    ResNeXt29_2x64d,
+    ResNeXt29_4x64d,
+    ResNeXt29_8x64d,
+    ResNeXt29_32x4d,
+)
+from .wide_resnet import WideResNet16_8
