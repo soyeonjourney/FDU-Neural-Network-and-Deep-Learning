c89897b88d16ee9b37e6003352be58652de6a751
Tequila-Sunrise <steven.shao6174@gmail.com>
2022-04-25 00:56:48 +0800

    Update train

--- Project2/code/cifar-10/train.sh
+++ Project2/code/cifar-10/train.sh
@@ -1,8 +1,9 @@
 CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNet18 --lr=0.1 --max-epoch=200
+# CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNet50 --lr=0.1 --max-epoch=200
 # CUDA_VISIBLE_DEVICES=0 python main.py --model=PreActResNet18 --lr=0.1 --max-epoch=200
-# CUDA_VISIBLE_DEVICES=0 python main.py --model=PreActResNet50 --lr=0.1 --max-epoch=200
 # CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNeXt29_32x4d --lr=0.1 --max-epoch=200
 # CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNeXt29_2x64d --lr=0.1 --max-epoch=200
 # CUDA_VISIBLE_DEVICES=0 python main.py --model=WideResNet28x10 --lr=0.1 --max-epoch=200
 # CUDA_VISIBLE_DEVICES=0 python main.py --model=DenseNet121 --lr=0.1 --max-epoch=200
+# CUDA_VISIBLE_DEVICES=0 python main.py --model=DPN26 --lr=0.1 --max-epoch=200
 # CUDA_VISIBLE_DEVICES=0 python main.py --model=DLA --lr=0.1 --max-epoch=200
