251afbee9c9a0e5451b4d56bfd8c0b7cbb7e6aac
Tequila-Sunrise <steven.shao6174@gmail.com>
2022-05-22 21:47:45 +0800

    feat: implement neural layers
    conv2d is still semi-manufacture, to be done later.

--- Project1/code/latte/functional.py
+++ Project1/code/latte/functional.py
@@ -1,5 +1,11 @@
 import numpy as np
+from typing import Tuple
 from latte.tensor import Tensor, Function
+
+
+#########################################################################################
+#                                        Function                                       #
+#########################################################################################
 
 
 def relu(input: 'Tensor') -> 'Tensor':
@@ -12,21 +18,42 @@ def sigmoid(input: 'Tensor') -> 'Tensor':
     return sigmoid_fn(input)
 
 
+def dropout(input: 'Tensor', p: float = 0.5, training: bool = True) -> 'Tensor':
+    dropout_fn = Dropout(p, training)
+    return dropout_fn(input)
+
+
+def conv2d(
+    input: 'Tensor',
+    weight: 'Tensor',
+    bias: 'Tensor',
+    stride: Tuple[int, int] = (1, 1),
+    padding: Tuple[int, int] = (0, 0),
+) -> 'Tensor':
+    conv2d_fn = Conv2d(stride, padding)
+    return conv2d_fn(input, weight, bias)
+
+
+#########################################################################################
+#                                         Class                                         #
+#########################################################################################
+
+
 class ReLU(Function):
     def __repr__(self) -> str:
         return 'Function(ReLU)'
 
-    def forward(self, a: 'Tensor') -> 'Tensor':
-        self.save_backward_node([a])
-        out = a.data
+    def forward(self, x: 'Tensor') -> 'Tensor':
+        self.save_backward_node([x])
+        out = x.data
         out[out < 0] = 0
-        return Tensor(out, grad_fn=self, requires_grad=a.requires_grad)
+        return Tensor(out, grad_fn=self, requires_grad=x.requires_grad)
 
     def backward(self, out: np.ndarray):
-        a = self.prev[0]
-        a.data[a.data <= 0] = 0
-        a.data[a.data > 0] = 1
-        a.grad = a.data * out
+        x = self.prev[0]
+        x.data[x.data <= 0] = 0
+        x.data[x.data > 0] = 1
+        x.grad = x.data * out
 
 
 class Sigmoid(Function):
@@ -36,12 +63,99 @@ class Sigmoid(Function):
     def sigmoid(self, x: np.ndarray) -> np.ndarray:
         return 1 / (1 + np.exp(-x))
 
-    def forward(self, a: 'Tensor') -> 'Tensor':
-        self.save_backward_node([a])
-        out = self.sigmoid(a.data)
-        return Tensor(out, grad_fn=self, requires_grad=a.requires_grad)
+    def forward(self, x: 'Tensor') -> 'Tensor':
+        self.save_backward_node([x])
+        out = self.sigmoid(x.data)
+        return Tensor(out, grad_fn=self, requires_grad=x.requires_grad)
 
     def backward(self, out: np.ndarray) -> None:
-        a = self.prev[0]
-        d_sigmoid = self.sigmoid(a.data) * (1 - self.sigmoid(a.data))
-        a.grad = d_sigmoid * out
+        x = self.prev[0]
+        d_sigmoid = self.sigmoid(x.data) * (1 - self.sigmoid(x.data))
+        x.grad = d_sigmoid * out
+
+
+class Dropout(Function):
+    def __init__(self, p: float = 0.5, training: bool = True) -> None:
+        super().__init__()
+        self.p = p
+        self.training = training
+        self.mask = None
+
+    def __repr__(self) -> str:
+        return 'Function(Dropout)'
+
+    def forward(self, x: 'Tensor') -> 'Tensor':
+        if self.training:
+            self.save_backward_node([x])
+            out = x.data
+            self.mask = np.random.binomial(1, self.p, size=x.shape) / (1 - self.p)
+            out = out * self.mask
+            return Tensor(out, grad_fn=self, requires_grad=x.requires_grad)
+
+        else:
+            return Tensor(x.data, grad_fn=self, requires_grad=x.requires_grad)
+
+    def backward(self, out: np.ndarray) -> None:
+        x = self.prev[0]
+        x.grad = self.mask * out
+
+
+class Conv2d(Function):
+    def __init__(
+        self, stride: Tuple[int, int] = (1, 1), padding: Tuple[int, int] = (0, 0)
+    ) -> None:
+        super().__init__()
+        self.stride = stride
+        self.padding = padding
+
+    def __repr__(self) -> str:
+        return 'Function(Conv2d)'
+
+    def forward(self, x: 'Tensor', weight: 'Tensor', bias: 'Tensor') -> 'Tensor':
+        self.save_backward_node([x, weight, bias])
+
+        batch_size, num_channels, image_h, image_w = x.shape
+        _, _, kernel_h, kernel_w = weight.shape
+        padding_h, padding_w = self.padding
+        stride_h, stride_w = self.stride
+        output_h = int((image_h + 2 * padding_h - kernel_h) / stride_h + 1)
+        output_w = int((image_w + 2 * padding_w - kernel_w) / stride_w + 1)
+
+        x = np.pad(
+            x,
+            (
+                (0, 0),
+                (0, 0),
+                (padding_h, padding_h + stride_h - 1),
+                (padding_w, padding_w + stride_w - 1),
+            ),
+            mode='constant',
+            constant_values=(0,),
+        )
+        x_tmp = np.zeros((batch_size, num_channels, kernel_h, kernel_w, output_h, output_w))
+        for i in range(kernel_h):
+            i_max = i + stride_h * output_h
+            for j in range(kernel_w):
+                j_max = j + stride_w * output_w
+                x_tmp[:, :, i, j, :, :] = x[:, :, i:i_max:stride_h, j:j_max:stride_w]
+
+        output = np.tensordot(x_tmp, weight.data, axes=([1, 2, 3], [1, 2, 3]))
+        if bias is not None:
+            output += bias.data
+
+        output = np.transpose(output, (0, 3, 1, 2))
+        return Tensor(output, grad_fn=self, requires_grad=x.requires_grad)
+
+    def backward(self, out: np.ndarray) -> None:
+        x, weight, bias = self.prev
+
+        batch_size, num_channels, image_h, image_w = x.shape
+        _, _, kernel_h, kernel_w = weight.shape
+
+        # Gradient of x
+        
+
+#########################################################################################
+#                                        Utility                                        #
+#########################################################################################
+
--- Project1/code/latte/nn.py
+++ Project1/code/latte/nn.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import List, Tuple
 from latte.tensor import Tensor, Function
+import latte.functional as F
 
 
 #########################################################################################
@@ -12,6 +13,7 @@ class Module:
     def __init__(self) -> None:
         self._modules = []
         self._params = []
+        self.training = True  # Training mode or test mode
 
     def __repr__(self) -> str:
         return 'Module()'
@@ -31,26 +33,40 @@ class Module:
             self._modules.append(__value)
             self._params.extend(__value.parameters())
         # object.__setattr__(self, __name, __value)
-        super(Module, self).__setattr__(__name, __value)
+        super().__setattr__(__name, __value)
+
+    def eval(self) -> None:
+        self.training = False
+
+    def train(self) -> None:
+        self.training = True
 
 
 class Linear(Module):
-    def __init__(self, in_features: int, out_features: int) -> None:
-        super(Linear, self).__init__()
+    def __init__(self, in_features: int, out_features: int, bias: bool = True) -> None:
+        super().__init__()
         self.weight = Tensor(
             np.random.randn(in_features, out_features) * 0.01, requires_grad=True
         )
-        self.bias = Tensor(np.zeros(out_features), requires_grad=True)
-        self._params.extend([self.weight, self.bias])
+        if bias:
+            self.bias = Tensor(
+                np.zeros((1, out_features)), requires_grad=True, is_bias=True
+            )
+            self._params.extend([self.weight, self.bias])
+        else:
+            self.bias = None
+            self._params.append(self.weight)
 
     def __repr__(self) -> str:
-        return f'Linear({self.weight.shape[0]}, {self.weight.shape[1]})'
+        return f'Linear({self.weight.shape[0]}, {self.weight.shape[1]}, bias={self.bias is not None})'
 
     def forward(self, input: 'Tensor') -> 'Tensor':
-        return input.dot(self.weight) + self.bias  # x @ W + b
+        if self.bias is not None:
+            return input.dot(self.weight) + self.bias  # x @ W + b
+        else:
+            return input.dot(self.weight)  # x @ W
 
 
-# TODO: complete conv2d
 class Conv2d(Module):
     def __init__(
         self,
@@ -59,16 +75,70 @@ class Conv2d(Module):
         kernel_size: Tuple[int, ...],
         stride: Tuple[int, ...] = (1, 1),
         padding: Tuple[int, ...] = (0, 0),
+        bias: bool = True,
     ) -> None:
-        super(Conv2d, self).__init__()
+        super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
+        self.kernel_size = _pair(kernel_size)
+        self.stride = _pair(stride)
+        self.padding = _pair(padding)
 
         # Initialize weight and bias
-        self.weight = Tensor()
+        scale = np.sqrt(
+            2.0 / (self.in_channels * self.kernel_size[0] * self.kernel_size[1])
+        )
+        self.weight = Tensor(
+            np.random.randn(self.out_channels, self.in_channels, *self.kernel_size)
+            * scale,
+            requires_grad=True,
+        )
+        if bias:
+            self.bias = Tensor(
+                np.zeros((1, self.out_channels, 1, 1)),
+                requires_grad=True,
+                is_bias=True,
+            )
+            self._params.extend([self.weight, self.bias])
+        else:
+            self.bias = None
+            self._params.append(self.weight)
+
+    def forward(self, input: 'Tensor') -> 'Tensor':
+        return F.conv2d(input, self.weight, self.bias, self.stride, self.padding)
+
+
+class Dropout(Module):
+    def __init__(self, p: float = 0.5) -> None:
+        super().__init__()
+        self.p = p
+
+    def __repr__(self) -> str:
+        return f'Dropout({self.p}, training={self.training})'
+
+    def forward(self, input: 'Tensor') -> 'Tensor':
+        return F.dropout(input, self.p, self.training)
+
+
+#########################################################################################
+#                                      Activation                                       #
+#########################################################################################
+
+
+class ReLU(Module):
+    def __repr__(self) -> str:
+        return 'Activation(ReLU)'
+
+    def forward(self, input: 'Tensor') -> 'Tensor':
+        return F.relu(input)
+
+
+class Sigmoid(Module):
+    def __repr__(self) -> str:
+        return 'Activation(Sigmoid)'
+
+    def forward(self, input: 'Tensor') -> 'Tensor':
+        return F.sigmoid(input)
 
 
 #########################################################################################
@@ -76,9 +146,29 @@ class Conv2d(Module):
 #########################################################################################
 
 
+class MSELoss(Function):
+    def __repr__(self) -> str:
+        return 'LossFunction(MSELoss)'
+
+    def forward(self, input: 'Tensor', target: 'Tensor') -> 'Tensor':
+        self.save_backward_node([input, target])
+        input_data = input.data
+        target_data = target.data
+        loss = (input_data - target_data) ** 2 / 2
+
+        return Tensor(np.mean(loss), grad_fn=self, requires_grad=input.requires_grad)
+
+    def backward(self, out: np.ndarray) -> None:
+        a, b = self.prev
+        m = a.shape[0]
+        input_data = a.data
+        target_data = b.data
+        a.grad = (input_data - target_data) / m
+
+
 class BCELoss(Function):
     def __repr__(self) -> str:
-        return 'Function(BCELoss)'
+        return 'LossFunction(BCELoss)'
 
     def forward(self, input: 'Tensor', target: 'Tensor') -> 'Tensor':
         self.save_backward_node([input, target])
@@ -96,16 +186,14 @@ class BCELoss(Function):
         m = a.size
         input_data = a.data
         target_data = b.data
-        a.grad = (
-            (-target_data / input_data + (1 - target_data) / (1 - input_data)) / m
-        )
+        a.grad = (-target_data / input_data + (1 - target_data) / (1 - input_data)) / m
 
 
 class CrossEntropyLoss(Function):
     """Combines log_softmax and nll_loss in a single function."""
 
     def __repr__(self) -> str:
-        return 'Function(CrossEntropyLoss)'
+        return 'LossFunction(CrossEntropyLoss)'
 
     def forward(self, input: 'Tensor', target: 'Tensor') -> 'Tensor':
         self.save_backward_node([input, target])
@@ -114,7 +202,7 @@ class CrossEntropyLoss(Function):
         target_data = target.data
 
         # softmax = exp(x[target]) / sum(exp(x[i]), axis=1)
-        neg_log_softmax = -input_data[range(m), target_data] + np.log(
+        neg_log_softmax = -input_data[np.arange(m), target_data] + np.log(
             np.sum(np.exp(input_data), axis=1)
         )
 
@@ -130,5 +218,18 @@ class CrossEntropyLoss(Function):
 
         # neg_log_softmax' = softmax - 1 * (i == target)
         softmax = np.exp(input_data) / np.sum(np.exp(input_data), axis=1).reshape(-1, 1)
-        softmax[range(m), target_data] -= 1
+        softmax[np.arange(m), target_data] -= 1
         a.grad = softmax / m
+
+
+#########################################################################################
+#                                        Utility                                        #
+#########################################################################################
+
+
+def _pair(x):
+    if np.isscalar(x):
+        return x, x
+    else:
+        assert len(x) == 2, "x must be a scalar or a pair"
+        return x
